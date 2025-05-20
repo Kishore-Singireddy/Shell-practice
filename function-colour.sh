46b2d5744b4e4848a82aaddb2f07d0b5b296ae2c
Kishore Singireddy <official4kishore@gmail.com>
2025-05-20 19:07:54 +0530

    updated the colour systenx

--- function-colour.sh
+++ function-colour.sh
@@ -9,6 +9,68 @@ YELLOW='\e[33m'
 BLUE='\e[34m'
 #Functions
 
-#VALIDATE(){}
+VALIDATE() { 
+    if [ $1 -ne 0 ]
+    then 
+        echo -e "${RED} $2 installation failed aborting the installation"
+        exit 1
+    else 
+        echo -e "${GREEN} $2 Installation is successful"
+    fi
+}
 
 echo -e "${YELLOW} Hi You are about to install ${BLUE} MYSQL, NGINX, PYTHON3 ${RESET}"
+echo "lets start the process"
+
+#checking if the user has root priviliages or not
+
+if [ $USERID -ne 0 ]
+then 
+    echo -e "${RED} You are not logged in as root user, you can not install, exiting.....${RESET}"
+    exit 1
+else 
+    echo -e "${GREEN} You have root access, proceedig to the next steps.... "
+
+if
+
+#Checking and installing the software
+
+
+dnf list installed mysql
+
+if [ $? -ne 0 ]
+then
+    echo  -e " ${GREEN} Installing MYSQL..."
+    dnf install mysql
+    VALIDATE $? "MYSQL"
+
+else 
+    echo -e "${GREEN} MYSQL is already installed, no action required."
+
+fi
+
+dnf list installed python3
+
+if [ $? -ne 0 ]
+then
+    echo  -e " ${GREEN} Installing PYTHON...."
+    dnf install python3
+    VALIDATE $? "PYTHON"
+
+else 
+    echo -e "${GREEN} PYTHON is already installed, no action required."
+
+fi
+
+dnf list installed nginx
+
+if [ $? -ne 0 ]
+then
+    echo  -e " ${GREEN} Installing nginx...."
+    dnf install nginx
+    VALIDATE $? "nginx"
+
+else 
+    echo -e "${GREEN} nginx is already installed, no action required."
+
+fi
