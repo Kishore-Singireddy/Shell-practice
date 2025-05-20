7af62d30558f498269b1e92ef19d361168fc617c
Kishore Singireddy <official4kishore@gmail.com>
2025-05-20 18:39:39 +0530

    updated the colour systenx

---
+++ function-colour.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+USERID=$(id -u)
+
+#Colours
+RED='\e[31m'
+GREEN='\e[32m'
+RESET='\e[0m'
+YELLOW='\e[33m'
+BLUE='\e[34m'
+#Functions
+
+#VALIDATE(){}
+
+echo "${YELLOW} Hi You are about to install ${BLUE} MYSQL, NGINX, PYTHON3 ${RESET}"
