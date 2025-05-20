ca92b07f18f20df9d0fb956971b4ca960f282540
Kishore Singireddy <official4kishore@gmail.com>
2025-05-20 17:33:33 +0530

    updating the colours.sh script by removing the single qoutes

--- colour.sh
+++ colour.sh
@@ -2,6 +2,6 @@
 
 echo "Hello world - No colour"
 
-echo -e "'e\[31m' Hello World - Red Colour"
+echo -e " e\[31m Hello World - Red Colour"
 echo -e "'e\[32m' Hellow World - Green Colour"
 echo "Hello World - No colour"
