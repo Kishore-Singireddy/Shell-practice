3be8ec3680fcab19aceff00837c75da8d3cda0d0
Kishore Singireddy <official4kishore@gmail.com>
2025-05-20 23:02:05 +0530

    updated the colour systenx

--- fn_clr_log.sh
+++ fn_clr_log.sh
@@ -10,12 +10,12 @@ BLUE='\e[34m'
 
 #for Log capture
 LOGS_FOLDER="/var/log/shellscript-logs"
-LOG_SCRIPT_NAME=$(echo $0 | cut -f "." -f1)
+LOG_SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOG_FILE=$($LOG_FOLDER/$LOG_SCRIPT_NAME.log)
 
 mkdir -p $LOG_FOLDER &>> $LOG_FILE
 
-echo "Script stated executing at $(date)" &>> $LOG_FILE
+echo "Script stated executing at $(date)" &>>$LOG_FILE
 
 #Functions
 
