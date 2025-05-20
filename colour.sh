f479b85508c5417a2480a911027b20bcea5e3d36
Kishore Singireddy <official4kishore@gmail.com>
2025-05-20 17:41:52 +0530

    updated the colour systenx

--- colour.sh
+++ colour.sh
@@ -3,5 +3,5 @@
 echo "Hello world - No colour"
 
 echo -e " \e[31m Hello World - Red Colour"
-echo -e "'\e[32m' Hellow World - Green Colour"
+echo -e " \e[32m Hello World - Green Colour \e[0m"
 echo "Hello World - No colour"
