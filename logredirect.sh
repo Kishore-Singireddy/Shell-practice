7458b00f8d028ba33c84beb9520f83ce3ce410f4
Kishore Singireddy <official4kishore@gmail.com>
2025-05-21 09:11:44 +0530

    Practise

--- logredirect.sh
+++ logredirect.sh
@@ -5,5 +5,13 @@ echo "Welcome for software installation"
 
 #Colour Variables"
 R='\e[31m'
+G='\e[32m'
+Y='\e[33m'
+C='\e[0m'
 
-echo -e "${R} Hello This is red "
+echo -e "${R} Hello This is red ${C}"
+echo -e "${G} Hello This is Green ${C} "
+echo -e "${Y} Hello This is Yellow ${C} "
+echo -e "${C} Hello This is clear ${C}"
+
+echo "Hello how are you ?"
